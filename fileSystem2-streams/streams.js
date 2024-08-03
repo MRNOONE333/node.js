e344aa8b67bba34d89df1a07e7cf052ba55552f5
haxAbdulAhad <abdul.ahad.21cse@bmu.edu.in>
2024-08-03 20:25:15 +0530

    node js file system Streams revision

--- fileSystem2-streams/streams.js
+++ fileSystem2-streams/streams.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 
 const readStream = fs.createReadStream('./docs/blog3.txt', { encoding: 'utf-8' });
-
+/*The 'data' term itself cannot be changed because it is a predefined event name in Node.js streams. 
+where as the the parameter chunk is just a variable name, can be aanything  xyz.....
+*/
 readStream.on('data', (chunk) => {
     console.log('---NEW CHUNK start---');
     console.log(chunk);
